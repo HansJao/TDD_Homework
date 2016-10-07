514445942c7c6a4f60de830872e4c2af8af430de
VisualStudy <vigor_han@hotmail.com>
2016-10-07 22:05:26 +0800

    HomeWork

--- Day1Homework/Day1Homework/Program.cs
+++ Day1Homework/Day1Homework/Program.cs
@@ -4,12 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day1Homework
+namespace Day1HomeWork
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+    public class CalculatorService
+    {
+        private ICountSum _sum;
+
+        public CalculatorService(ICountSum sum)
+        {
+            this._sum = sum;
+        }
+
+        public int[] DoCountSum(List<Product> ProductData, int GroupNumber, string FiledName)
+        {
+            return _sum.CountSum(ProductData, GroupNumber, FiledName);
+        }
+    }
+
+    public class CalculatorSum : ICountSum
+    {
+        public int[] CountSum(List<Product> ProductData, int GroupNumber, string FiledName)
+        {
+            //do something
+            return new int[] { 6, 15, 24, 21 };
+        }
+    }
+
+    public interface ICountSum
+    {
+        int[] CountSum(List<Product> ProductData, int GroupNumber, string FiledName);
+    }
+
+    public class Product
+    {
+        public int ID { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
     }
 }
---
+++ Day1Homework/Day1HomeworkTests/CalculatorServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day1HomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using ExpectedObjects;
+
+namespace Day1HomeWork.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void TestCostSum()
+        {
+            //arrange
+            var ProductData = GetProductData();
+
+            ICountSum calculator = new CalculatorSum();//Substitute.For<ICountSum>();
+            var expected = new int[] { 6, 15, 24, 21 };
+
+            //calculator.CountSum(ProductData,3,"Cost").Returns(expected);
+            CalculatorService calcultorService = new CalculatorService(calculator);
+
+            var actual = calculator.CountSum(ProductData, 3, "Cost");
+            //assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod()]
+        public void CountRevenusSum()
+        {
+            var ProductData = GetProductData();
+
+            ICountSum calculator = Substitute.For<ICountSum>();//new CalculatorSum();
+            var expected = new int[] { 50, 66, 60 };
+
+            calculator.CountSum(ProductData, 4, "Revenue").Returns(new int[] { 50, 66, 60 });
+            CalculatorService calcultorService = new CalculatorService(calculator);
+            //
+            expected.ToExpectedObject().ShouldEqual(calculator.CountSum(ProductData, 4, "Revenue"));
+        }
+
+        public List<Product> GetProductData()
+        {
+            var ProductData = new List<Product>
+            {
+                new Product {ID=1,Cost=1,Revenue=11,SellPrice=21 },
+                new Product {ID=2,Cost=2,Revenue=12,SellPrice=22 },
+                new Product {ID=3,Cost=3,Revenue=13,SellPrice=23 },
+                new Product {ID=4,Cost=4,Revenue=14,SellPrice=24 },
+                new Product {ID=5,Cost=5,Revenue=15,SellPrice=25 },
+                new Product {ID=6,Cost=6,Revenue=16,SellPrice=26 },
+                new Product {ID=7,Cost=7,Revenue=17,SellPrice=27 },
+                new Product {ID=8,Cost=8,Revenue=18,SellPrice=28 },
+                new Product {ID=9,Cost=9,Revenue=19,SellPrice=29 },
+                new Product {ID=10,Cost=10,Revenue=20,SellPrice=30 },
+                new Product {ID=11,Cost=11,Revenue=21,SellPrice=31 },
+
+            };
+
+            return ProductData;
+        }
+    }
+}
